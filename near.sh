e937b76551e3a294d9c57e83b094c0073af03d76
Zenuso <126380932+Zenuso@users.noreply.github.com>
2024-06-09 01:01:13 +0100

    Update near.sh

--- near.sh
+++ near.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://near.lava.build/lava-referer-00c6ad4a-5ae1-4fcd-b0ab-703adcd0635e/"
+RPC_URL="https://near.lava.build/lava-referer-3152fc09-615a-4797-82be-d14ce68df7b7/"
 
 OUTPUT_DIR="logs"
 
